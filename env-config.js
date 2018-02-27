c86b9bbfae159ed266448d711f1bbd13ceddc45f
slavik10 <dmitrsl10@gmail.com>
2018-02-27 22:17:27 +0200

    настроил deploy

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? 'belkanstrelka.com' : ''
+  'process.env.BACKEND_URL': prod ? 'http://belkanstrelka.com' : ''
 }
--- next.config.js
+++ next.config.js
@@ -11,5 +11,4 @@ module.exports = {
       '/about': { page: '/about' }
     }
   },
-  assetPrefix: !debug ? 'belkanstrelka.com' : ''
 }
