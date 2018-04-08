488e23b1f16bd9f2614bb310e657a84a2f7737d6
slavik10 <dmitrsl10@gmail.com>
2018-04-09 02:04:07 +0300

    add mobile-web-app-capable

--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,14 @@ export default class MyDocument extends Document {
           <title>Product design studio from Helsinki.</title>
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes">
 
           <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800,900&subset=latin,cyrillic" rel="stylesheet" />
           <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,600,700,800,900&subset=cyrillic,latin" rel="stylesheet" />
           <link href="//fonts.googleapis.com/css?family=Lato:300,400,500,600,700,800,900&subset=cyrillic,latin" rel="stylesheet" />
 
-          <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="stylesheet" href="/_next/static/style.css?ab=10" />
         </Head>
 
         <body>
