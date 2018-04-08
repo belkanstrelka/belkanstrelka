caae4cb15c23e5ef7a75a04af520de173d60c317
slavik10 <dmitrsl10@gmail.com>
2018-04-09 02:06:42 +0300

    hot fix

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="mobile-web-app-capable" content="yes">
+          <meta name="mobile-web-app-capable" content="yes" />
 
           <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800,900&subset=latin,cyrillic" rel="stylesheet" />
           <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,600,700,800,900&subset=cyrillic,latin" rel="stylesheet" />
