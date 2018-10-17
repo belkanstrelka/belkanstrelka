c4c11d1cf8d0c14d4c02fc580f43764b971dd585
slavik10 <dmitrsl10@gmail.com>
2018-10-17 09:51:12 -0700

    Deploy belkanstrelka

---
+++ docs/_next/static/chunks/styles.js
@@ -0,0 +1,232 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+
+/***/ "./src/js/modules/_layouts/flex/style.scss":
+/*!*************************************************!*\
+  !*** ./src/js/modules/_layouts/flex/style.scss ***!
+  \*************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"container":"container___1347H","container-full":"container-full___Z-cmg","grid":"grid___v8T-c","col-12":"col-12___2ewk8","col-11":"col-11___3hAFv","col-10":"col-10___32kI7","col-9":"col-9___WzTob","col-8":"col-8___3tnXO","col-7":"col-7___2mPYo","col-6":"col-6___CdAyj","col-5":"col-5___m_B8q","col-4":"col-4___3hfbZ","col-3":"col-3___5lrJc","col-2":"col-2___1DWAx","col-1":"col-1___s3Hug","col-xs-12":"col-xs-12___3bVVe","col-xs-11":"col-xs-11___p2tMQ","col-xs-10":"col-xs-10___hiFZN","col-xs-9":"col-xs-9___2kAUs","col-xs-8":"col-xs-8___3JDgo","col-xs-7":"col-xs-7___x-Gpx","col-xs-6":"col-xs-6___1tPkW","col-xs-5":"col-xs-5___g-fEw","col-xs-4":"col-xs-4___y6ndb","col-xs-3":"col-xs-3___3Umdu","col-xs-2":"col-xs-2___2IFN8","col-xs-1":"col-xs-1___1zyoQ","col-sm-12":"col-sm-12___AxYF8","col-sm-11":"col-sm-11___3_7n8","col-sm-10":"col-sm-10___3IfwD","col-sm-9":"col-sm-9___35N08","col-sm-8":"col-sm-8___pvK1m","col-sm-7":"col-sm-7___FIYTG","col-sm-6":"col-sm-6___OCZPn","col-sm-5":"col-sm-5___3a81i","col-sm-4":"col-sm-4___3tTVw","col-sm-3":"col-sm-3___ggNg7","col-sm-2":"col-sm-2___1xZ8U","col-sm-1":"col-sm-1___dUnAD","col-md-12":"col-md-12___BG72K","col-md-11":"col-md-11___vsrLM","col-md-10":"col-md-10___2eX8O","col-md-9":"col-md-9___1FZ8k","col-md-8":"col-md-8___31JL9","col-md-7":"col-md-7___LWzmr","col-md-6":"col-md-6___--DU5","col-md-5":"col-md-5___23lOJ","col-md-4":"col-md-4___32Ta4","col-md-3":"col-md-3___3Gk9s","col-md-2":"col-md-2___33mHA","col-md-1":"col-md-1___295w-","col-lg-12":"col-lg-12___3vqo7","col-lg-11":"col-lg-11___vOolR","col-lg-10":"col-lg-10___pzHSn","col-lg-9":"col-lg-9___2Iz7E","col-lg-8":"col-lg-8___2n9rl","col-lg-7":"col-lg-7___2WVyN","col-lg-6":"col-lg-6___2OQTV","col-lg-5":"col-lg-5___AXrwK","col-lg-4":"col-lg-4___1Uy98","col-lg-3":"col-lg-3___23-aI","col-lg-2":"col-lg-2___fhY-z","col-lg-1":"col-lg-1___3GRSe","col-xlg-12":"col-xlg-12___1B-OM","col-xlg-11":"col-xlg-11___Y2dwS","col-xlg-10":"col-xlg-10___2avn8","col-xlg-9":"col-xlg-9___1PJKX","col-xlg-8":"col-xlg-8___1sKlr","col-xlg-7":"col-xlg-7___2UA2t","col-xlg-6":"col-xlg-6___1tMSD","col-xlg-5":"col-xlg-5___wakpL","col-xlg-4":"col-xlg-4___3XT9V","col-xlg-3":"col-xlg-3___3oYGz","col-xlg-2":"col-xlg-2___19GKj","col-xlg-1":"col-xlg-1___3HwMV","col-auto":"col-auto___2yjGf","col-xs-auto":"col-xs-auto___3S_o8","col-sm-auto":"col-sm-auto___39FbY","col-md-auto":"col-md-auto___P3R4v","col-lg-auto":"col-lg-auto___2qyrP","col-xlg-auto":"col-xlg-auto___1P33u","order-12":"order-12___2FBPS","order-11":"order-11___3vtJh","order-10":"order-10___1KtYZ","order-9":"order-9___2Rff6","order-8":"order-8___1anSa","order-7":"order-7___34SKT","order-6":"order-6___2UWwO","order-5":"order-5___rRMak","order-4":"order-4___t92Iv","order-3":"order-3___-op64","order-2":"order-2___3c_9s","order-1":"order-1___2zb5I","order-0":"order-0___2lRA_","order-12-xs":"order-12-xs___1mpnq","order-11-xs":"order-11-xs___2r0O4","order-10-xs":"order-10-xs___3dfUO","order-9-xs":"order-9-xs___2JKOY","order-8-xs":"order-8-xs___u-J_t","order-7-xs":"order-7-xs___34peS","order-6-xs":"order-6-xs___27o8u","order-5-xs":"order-5-xs___1n4lw","order-4-xs":"order-4-xs___wyzmJ","order-3-xs":"order-3-xs___kc7Q3","order-2-xs":"order-2-xs___37yrF","order-1-xs":"order-1-xs___3YW47","order-0-xs":"order-0-xs___2XyWs","order-12-sm":"order-12-sm___2cZIm","order-11-sm":"order-11-sm___bG2ps","order-10-sm":"order-10-sm___dgbog","order-9-sm":"order-9-sm___17pgD","order-8-sm":"order-8-sm___3k_CM","order-7-sm":"order-7-sm___1AvD0","order-6-sm":"order-6-sm___33rFE","order-5-sm":"order-5-sm___a5Frm","order-4-sm":"order-4-sm___3pYDx","order-3-sm":"order-3-sm___38fl-","order-2-sm":"order-2-sm___plqMf","order-1-sm":"order-1-sm___biwVw","order-0-sm":"order-0-sm___1hA6m","order-12-md":"order-12-md___3TeuC","order-11-md":"order-11-md___IHYk_","order-10-md":"order-10-md___34CUv","order-9-md":"order-9-md___v4Ine","order-8-md":"order-8-md___2QirA","order-7-md":"order-7-md___2JmsA","order-6-md":"order-6-md___3vJQu","order-5-md":"order-5-md___IQBYf","order-4-md":"order-4-md___3r2Vb","order-3-md":"order-3-md___sHtkJ","order-2-md":"order-2-md___3ybGU","order-1-md":"order-1-md___3U0aG","order-0-md":"order-0-md___1agDK","order-12-lg":"order-12-lg___22ard","order-11-lg":"order-11-lg___BeWyW","order-10-lg":"order-10-lg___2zxDM","order-9-lg":"order-9-lg___3hPlA","order-8-lg":"order-8-lg___30ThB","order-7-lg":"order-7-lg___3vXbC","order-6-lg":"order-6-lg___2o2Tc","order-5-lg":"order-5-lg___1y1JA","order-4-lg":"order-4-lg___9o1ej","order-3-lg":"order-3-lg___Y-Jyn","order-2-lg":"order-2-lg___1SFFB","order-1-lg":"order-1-lg___20Tpt","order-0-lg":"order-0-lg___Ou8EZ","order-12-xlg":"order-12-xlg___Bru18","order-11-xlg":"order-11-xlg___1pPcu","order-10-xlg":"order-10-xlg___18_6P","order-9-xlg":"order-9-xlg___zznla","order-8-xlg":"order-8-xlg___3yh3b","order-7-xlg":"order-7-xlg___Wxcvz","order-6-xlg":"order-6-xlg___3mjGm","order-5-xlg":"order-5-xlg___3AxHC","order-4-xlg":"order-4-xlg___s_HO-","order-3-xlg":"order-3-xlg___3BNYO","order-2-xlg":"order-2-xlg___3MqPg","order-1-xlg":"order-1-xlg___36Sx1","order-0-xlg":"order-0-xlg___noFO5","wrap":"wrap___2qndD","no-wrap":"no-wrap___CdK10","wrap-reverse":"wrap-reverse___2Grt-","direction-row":"direction-row___1g-rO","direction-row-reverse":"direction-row-reverse___2KXvJ","direction-column":"direction-column___2tZC5","direction-column-reverse":"direction-column-reverse___5U7hG","align-start":"align-start___12eu1","align-end":"align-end___St0yn","align-center":"align-center___2Kp7u","align-baseline":"align-baseline___1Aq5g","align-content-start":"align-content-start___2xoqE","align-content-end":"align-content-end___8er0r","align-content-center":"align-content-center___3q7Rs","align-content-space-between":"align-content-space-between___3Gn1h","align-content-space-around":"align-content-space-around___2rZGT","align-self-stretch":"align-self-stretch___2egTj","align-self-start":"align-self-start___2isNq","align-self-end":"align-self-end___3jPDF","align-self-center":"align-self-center___3h6Lt","align-self-baseline":"align-self-baseline___2FVSQ","justify-start":"justify-start___2tbDQ","justify-end":"justify-end___1qMW7","justify-center":"justify-center___3CYFx","justify-space-between":"justify-space-between___1ohfF","justify-space-around":"justify-space-around___2UFZy","grid-bleed":"grid-bleed___1rcb-","col-grid":"col-grid___2HmW_","col-bleed":"col-bleed___o6kBg","col-bleed-x":"col-bleed-x___1ANig","col-bleed-y":"col-bleed-y___26Upq","flex-img":"flex-img___1LuCk","flex-footer":"flex-footer___nIiVT","hidden-xxs":"hidden-xxs___bnjO6","hidden-xs":"hidden-xs___2auz2","hidden-sm":"hidden-sm___2oxiU","hidden-md":"hidden-md___26Adz","hidden-lg":"hidden-lg___fJ0AU","flexHorizontal":"flexHorizontal___BKTNk","flexVertical":"flexVertical___35C9Z","flexSpacer":"flexSpacer___2J7t1"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1539794312044");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./src/js/modules/common/btn/style.scss":
+/*!**********************************************!*\
+  !*** ./src/js/modules/common/btn/style.scss ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"btn":"btn___1bgKy","btn--hover":"btn--hover___1BKKv","btn__inner":"btn__inner___1_vF4","btn-wrapper":"btn-wrapper___3jGPx","btn__arrow":"btn__arrow___1pMEU","btn__text":"btn__text___1OR4a","Link__target-blank":"Link__target-blank___2JzxU","btn--arrow":"btn--arrow___yxRYx","btn--small":"btn--small___3WkWr","btn--bordered":"btn--bordered___2kUk1","btn--active":"btn--active___2d5p8","btn--white":"btn--white___1nkIT","btn--dark":"btn--dark___1Xrbn","btn--uppercase":"btn--uppercase___27iH4","btn--danger":"btn--danger___2Nn1L","btn--success":"btn--success___2TtMa","btn--secondary":"btn--secondary___2ryYX"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1539794311100");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./src/js/modules/common/footer/footer.style.scss":
+/*!********************************************************!*\
+  !*** ./src/js/modules/common/footer/footer.style.scss ***!
+  \********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"footer":"footer___3g7EQ","footer__address":"footer__address___2_Dja","footer__title":"footer__title___3xmHY","footer__copyright":"footer__copyright___3NqAK","footer__separeteLine":"footer__separeteLine___sKZ16","footer__social":"footer__social___36-ww","footer__payment":"footer__payment___2lgxI","footer__link":"footer__link___3pMyn","footer__line":"footer__line___1byQC","footer__icon":"footer__icon___1j2Q5","contactFooter":"contactFooter___2yeLW","contactFooter__form":"contactFooter__form___398EC","contactFooter__emails":"contactFooter__emails___x6ZZa","contactFooter__title":"contactFooter__title___2HSQp","contactFooter__text":"contactFooter__text___VNDi5","contactFooter__subtitle":"contactFooter__subtitle___2x0b6","contactFooter__email":"contactFooter__email___1EOdo"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1539794311187");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./src/js/modules/common/header/views/bnsv1/header.style.scss":
+/*!********************************************************************!*\
+  !*** ./src/js/modules/common/header/views/bnsv1/header.style.scss ***!
+  \********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"breadcrumbs":"breadcrumbs___3YCMa","header":"header___2JlV2","header__submenu":"header__submenu___12Jgv","header__submenuMenu":"header__submenuMenu___2PGY7","isActive":"isActive___3XpR-","header__wrapper":"header__wrapper___2crh0","header__logo":"header__logo___2DHIF","header__separator":"header__separator___1TQHV","header__title":"header__title___rVIm5"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1539794312568");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./src/js/modules/landings/index/style.scss":
+/*!**************************************************!*\
+  !*** ./src/js/modules/landings/index/style.scss ***!
+  \**************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"parallaxChildren":"parallaxChildren___3gye6","sideText":"sideText___3ByIS","sideText__abs":"sideText__abs___3FstA","forWhom":"forWhom___28nQg","forWhom__item":"forWhom__item___2Cn9u","forWhom__icon":"forWhom__icon___e1Fvc","forWhom__description":"forWhom__description___2zxAz","proceccItem":"proceccItem___1oRbj","proceccItem__title":"proceccItem__title___35vPQ","proceccItem__separator":"proceccItem__separator___2x7pe","proceccItem__text":"proceccItem__text___3zqjX","bnsIndexService__info":"bnsIndexService__info___3EcpP","bnsIndexService__ps":"bnsIndexService__ps___12q5R","bnsIndexService__bold":"bnsIndexService__bold___1t6zU","bnsIndexService":"bnsIndexService___2AFkj","bnsIndexService__statBlock":"bnsIndexService__statBlock___17b5W","bnsIndexService__statTitle":"bnsIndexService__statTitle___tyRyH","bnsIndexService__statsDescription":"bnsIndexService__statsDescription___3fAWr","onDemandItem":"onDemandItem___2ySOM","onDemandItem__icon":"onDemandItem__icon___33at-","onDemandItem__title":"onDemandItem__title___C0dPW","onDemandItem__text":"onDemandItem__text___EIP1h","bnsIndex":"bnsIndex___8JcSD","bnsIndex__sticky":"bnsIndex__sticky___3Grye","bnsIndex__heroPlanet":"bnsIndex__heroPlanet___kEc8b","bnsIndex__title":"bnsIndex__title___2pt3l","bnsIndex__description":"bnsIndex__description___2Gn-p","bnsIndex__WYSIWYG":"bnsIndex__WYSIWYG___l4GZ0","bnsIndex__misson":"bnsIndex__misson___3Anx4","bnsIndex__onDemand":"bnsIndex__onDemand___1GM6g","bnsIndex__hero":"bnsIndex__hero___2kMgF","bnsIndex__expertise":"bnsIndex__expertise___1aBDC","bnsIndex__service":"bnsIndex__service___2VVLX","bnsIndex__process":"bnsIndex__process___1o9gX","bnsIndex__forWhom":"bnsIndex__forWhom___1pzlt","bnsIndex__onDemandItems":"bnsIndex__onDemandItems___1CZMa","bnsIndex__expertiseBlocks":"bnsIndex__expertiseBlocks___UzHsH","bnsIndex__expertiseBlock":"bnsIndex__expertiseBlock___1mHCM"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1539794310688");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"bnsContainer":"bnsContainer___1UN__","bnsContainer_w720":"bnsContainer_w720___1CRbI","bnsContainer_w860":"bnsContainer_w860___257zn"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1539794236437");
+          });
+      }
+    }
+  
+
+/***/ })
+
+}]);
+//# sourceMappingURL=styles.js.map
---
+++ docs/_next/static/webpack/styles.a48b9ffaf5bcfc5356c1.hot-update.js
@@ -0,0 +1,34 @@
+webpackHotUpdate("styles",{
+
+/***/ "./src/js/modules/_layouts/flex/style.scss":
+false,
+
+/***/ "./src/js/modules/common/btn/style.scss":
+false,
+
+/***/ "./src/js/modules/common/footer/footer.style.scss":
+false,
+
+/***/ "./src/js/modules/common/header/views/bnsv1/header.style.scss":
+false,
+
+/***/ "./src/js/modules/landings/index/style.scss":
+false,
+
+/***/ 5:
+false,
+
+/***/ 6:
+false,
+
+/***/ 7:
+false,
+
+/***/ 8:
+false,
+
+/***/ 9:
+false
+
+})
+//# sourceMappingURL=styles.a48b9ffaf5bcfc5356c1.hot-update.js.map
